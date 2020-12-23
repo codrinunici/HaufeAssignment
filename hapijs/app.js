2bf97a0b32fa12adfe248aebea3ea94dc54f34dd
Codrin Unici <unicicodrin@Gmail.com>
2020-12-23 19:06:03 +0200

    Added MongoDB connection to Hapi with first two endpoints

--- hapijs/app.js
+++ hapijs/app.js
@@ -1,22 +1,52 @@
-
 const Hapi = require('@hapi/hapi');
 const Mongoose = require('mongoose');
 
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+const connection = Mongoose.connect("mongodb://localhost:test", {useNewUrlParser: true});
+
 const init = async () => {
-
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
-
     await server.start();
     console.log('Server running on %s', server.info.uri);
 };
 
-process.on('unhandledRejection', (err) => {
 
+process.on('unhandledRejection', (err) => {
     console.log(err);
     process.exit(1);
 });
 
 init();
+const salutationSchema = new Mongoose.Schema({
+    salutation: String
+});
+const GreetingsModel = Mongoose.model("greeting", salutationSchema);
+
+server.route({
+    method: "POST",
+    path: "/greeting",
+    handler: async (request, h) => {
+        try {
+            let salutation = new GreetingsModel({salutation: JSON.parse(request.payload).salutation});
+            let result = await salutation.save();
+            return h.response(result);
+        } catch (error) {
+            return h.response(error.message).code(300);
+        }
+    }
+});
+
+server.route({
+    method: "GET",
+    path: "/",
+    handler: async (request, h) => {
+        try {
+            return Mongoose.connection.readyState === 1 ? h.response("That Mongo fella is still with us") : h.response("Mongo's DOWN");
+        } catch (error) {
+            return h.response(error.message).code(300);
+        }
+    }
+});
